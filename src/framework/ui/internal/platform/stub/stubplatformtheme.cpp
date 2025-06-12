d237744c6e591de9b7b6dfd86af25cc77898e05d
githubwbp1988 <wbp123@yeah.net>
2025-06-12 10:58:13 +0800

    test wasm dark window theme

--- src/framework/ui/internal/platform/stub/stubplatformtheme.cpp
+++ src/framework/ui/internal/platform/stub/stubplatformtheme.cpp
@@ -25,6 +25,8 @@
 using namespace muse::ui;
 using namespace muse::async;
 
+class QColor;
+
 void StubPlatformTheme::startListening()
 {
 }
@@ -57,6 +59,10 @@ void StubPlatformTheme::applyPlatformStyleOnAppForTheme(const ThemeCode&)
 {
 }
 
-void StubPlatformTheme::applyPlatformStyleOnWindowForTheme(QWindow*, const ThemeCode&)
+void StubPlatformTheme::applyPlatformStyleOnWindowForTheme(QWindow* window, const ThemeCode&)
 {
+    if (!window) {
+        return;
+    }
+    window->setColor(QColor(50, 50, 50));
 }
