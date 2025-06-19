543fa139d3463509b7d45ed233b49612eee47116
githubwbp1988 <wbp123@yeah.net>
2025-06-20 00:26:15 +0800

    wasm debug

--- src/webbridge/internal/webinteractive.h
+++ src/webbridge/internal/webinteractive.h
@@ -80,7 +80,7 @@ public:
                                                 const muse::io::paths_t& selectedDirectories) override;
 
     // color
-    async::Promise<Color> selectColor(const QColor& color = Qt::white, const std::string& title = "") override;
+    muse::async::Promise<Color> selectColor(const QColor& color = Qt::white, const std::string& title = "") override;
     bool isSelectColorOpened() const override;
 
     // custom
