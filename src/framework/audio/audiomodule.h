6ea153e941393ea09fa4d57dbbe4ebbd4675efa8
githubwbp1988 <wbp123@yeah.net>
2025-06-21 12:30:40 +0800

    debug

--- src/framework/audio/audiomodule.h
+++ src/framework/audio/audiomodule.h
@@ -50,7 +50,7 @@ class RegisterAudioPluginsScenario;
 class AudioModule : public modularity::IModuleSetup, public async::Asyncable
 {
 public:
-    AudioModule();
+    AudioModule() override;
 
     std::string moduleName() const override;
 
