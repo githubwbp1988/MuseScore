2c12acd9767722020f42516c4eb0b71d40094dbe
githubwbp1988 <wbp123@yeah.net>
2025-06-24 18:47:18 +0800

    wasm build debug

--- buildscripts/ci/wasm/build.sh
+++ buildscripts/ci/wasm/build.sh
@@ -59,20 +59,20 @@ source $BUILD_TOOLS/environment.sh
 
 export CMAKE_TOOLCHAIN_FILE=${QT_ROOT_DIR}/lib/cmake/Qt6/qt.toolchain.cmake
 
-# # =========== Build =======================
-# echo "=== BUILD ==="
+# =========== Build =======================
+echo "=== BUILD ==="
 
-# MUSESCORE_REVISION=$(git rev-parse --short=7 HEAD)
+MUSESCORE_REVISION=$(git rev-parse --short=7 HEAD)
 
-# # Build portable AppImage
-# MUSE_APP_BUILD_MODE=$MUSE_APP_BUILD_MODE \
-# MUSESCORE_BUILD_CONFIGURATION="app-web" \
-# MUSESCORE_BUILD_NUMBER=$BUILD_NUMBER \
-# MUSESCORE_REVISION=$MUSESCORE_REVISION \
-# bash ./ninja_build.sh -t release
+# Build portable AppImage
+MUSE_APP_BUILD_MODE=$MUSE_APP_BUILD_MODE \
+MUSESCORE_BUILD_CONFIGURATION="app-web" \
+MUSESCORE_BUILD_NUMBER=$BUILD_NUMBER \
+MUSESCORE_REVISION=$MUSESCORE_REVISION \
+bash ./ninja_build.sh -t release
 
-# bash ./buildscripts/ci/tools/make_release_channel_env.sh -c $MUSE_APP_BUILD_MODE
-# bash ./buildscripts/ci/tools/make_version_env.sh $BUILD_NUMBER
-# bash ./buildscripts/ci/tools/make_revision_env.sh $MUSESCORE_REVISION
-# bash ./buildscripts/ci/tools/make_branch_env.sh
+bash ./buildscripts/ci/tools/make_release_channel_env.sh -c $MUSE_APP_BUILD_MODE
+bash ./buildscripts/ci/tools/make_version_env.sh $BUILD_NUMBER
+bash ./buildscripts/ci/tools/make_revision_env.sh $MUSESCORE_REVISION
+bash ./buildscripts/ci/tools/make_branch_env.sh
 
