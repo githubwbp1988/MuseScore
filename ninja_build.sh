a4c2f041e511662a65788f48060c2898bdb84db8
githubwbp1988 <wbp123@yeah.net>
2025-06-26 11:08:31 +0800

    debug

--- ninja_build.sh
+++ ninja_build.sh
@@ -106,8 +106,7 @@ function do_build() {
         -DLIBSND_PATH=${GITHUB_WORKSPACE}/wasm-thirdparty-src/libsndfile \
         -DLIBOGG_PATH=${GITHUB_WORKSPACE}/wasm-thirdparty-src/libogg \
         -DLIBVORBIS_PATH=${GITHUB_WORKSPACE}/wasm-thirdparty-src/libvorbis \
-        # -DGLIB_PATH=${GITHUB_WORKSPACE}/wasm-thirdparty-built/glib \
-        -DGLIB_PATH=${GITHUB_WORKSPACE}/wasm-thirdparty-built/glib-emscripten \
+        -DGLIB_PATH=${GITHUB_WORKSPACE}/wasm-thirdparty-built/glib \
         -DCMAKE_CXX_FLAGS="-g -gsource-map" \
         -DCMAKE_C_FLAGS="-g -gsource-map" 
 
